df534d2d38577a3207656254496141b08e0b876c
Julien Lamoureux <julien.lamoureux@stu.ocsb.ca>
2023-09-18 20:58:31 +0000

    Updated basic_math.py

--- basic_math.py
+++ basic_math.py
@@ -2,7 +2,7 @@
 
 # Created By: Julien Lamoureux
 # Date: September 18, 2023
-# This program displays basic math operations
+# This program displays basic math in Python
 
 
 def main():
@@ -10,6 +10,7 @@ def main():
     print("4-3 = {}".format(4 - 3))
     print("4*3 = {}".format(4 * 3))
     print("4/3 = {}".format(4 / 3))
+    print("4^3 = {}".format(4 ^ 3))
     print("4**3 = {}".format(4**3))
     print("4**0.5 = {}".format(4**0.5))
 
