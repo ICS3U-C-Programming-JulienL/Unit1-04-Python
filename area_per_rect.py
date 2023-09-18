0ef52db248fb93b46cf302216adff5f0128b4854
Julien Lamoureux <julien.lamoureux@stu.ocsb.ca>
2023-09-18 17:03:16 +0000

    Added area_per_rect.py

---
+++ area_per_rect.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# Created By: Julien Lamoureux
+# Date: September 18, 2023
+# This program displays basic math operations
+
+
+def main():
+    print("If a rectangle has the dimensions:")
+    print("6 cm x 2 cm")
+    print("The area is {} cm^2".format(6 * 2))
+    print("The perimeter {} cm".format(2 * (6 + 2)))
+
+
+if __name__ == "__main__":
+    main()
---
+++ basic_math.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# Created By: Julien Lamoureux
+# Date: September 18, 2023
+# This program displays basic math operations
+
+
+def main():
+    print("4+3 = {}".format(4 + 3))
+    print("4-3 = {}".format(4 - 3))
+    print("4*3 = {}".format(4 * 3))
+    print("4/3 = {}".format(4 / 3))
+    print("4**3 = {}".format(4**3))
+    print("4**0.5 = {}".format(4**0.5))
+
+
+if __name__ == "__main__":
+    main()
